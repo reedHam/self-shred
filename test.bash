39ee42bf7cb970a0f8bfe26dfdf6c805fdd235f8
Reed Hambrook <reed.hambrook@gmail.com>
2022-05-28 01:35:01 -0700

    updated self-shred.bash refrence

--- test.bash
+++ test.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp sensitive-script.bash ./sensitive-script-test.bash
-./sensitive-script-test.bash
-[ ! -f sensitive-script-test.bash ] && echo "Its gone!" && exit 1
+cp self-shred.bash ./self-shred-test.bash
+./self-shred-test.bash
+[ ! -f self-shred-test.bash ] && echo "Its gone!" && exit 1
 echo "FAILED"
