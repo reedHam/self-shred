9ecc8b5f8b3e87ba9c0545ebc606dfd046cd3560
Reed Hambrook <reed.hambrook@gmail.com>
2022-05-28 01:01:39 -0700

    fixed prompt

--- sensitive-script.bash
+++ sensitive-script.bash
@@ -2,7 +2,7 @@
 echo "my password";
 
 SHREDDING_GRACE_SECONDS=${SHREDDING_GRACE_SECONDS:-5}
-(( $SHREDDING_GRACE_SECONDS > 0 )) && echo "Shreding in $SHREDDING_GRACE_SECONDS seconds"
+(( $SHREDDING_GRACE_SECONDS > 0 )) && echo -e "Shreding ${0} in $SHREDDING_GRACE_SECONDS seconds \e[1;31mCTRL-C TO KEEP FILE\e[0m"
 BOMB="●"
 FUZE='~'
 SPARK="\e[1;31m*\e[0m"
